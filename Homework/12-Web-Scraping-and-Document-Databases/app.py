239e0e5507966de22127b5fb93b006c89ddcb3d9
Dani G <garciadanielle5@gmail.com>
2021-05-05 21:19:53 -0400

    cleaning up code

--- Homework/12-Web-Scraping-and-Document-Databases/app.py
+++ Homework/12-Web-Scraping-and-Document-Databases/app.py
@@ -2,18 +2,11 @@ from flask import Flask, render_template, redirect
 import pymongo
 import scrape_mars
 
-# Use PyMongo to establish Mongo connection
 client = pymongo.MongoClient('mongodb://localhost:27017')
 db = client.mars_db
 collection = db.mars
-# Create an instance of Flask
 app = Flask(__name__)
 
-
-
-
-
-# Route to render index.html template using data from Mongo
 @app.route('/')
 def home():
 	mars = collection.find_one()
@@ -25,7 +18,5 @@ def scrape():
 	return redirect('/', code = 302)
 
  
-
-
 if __name__ == "__main__":
     app.run(debug=True)
