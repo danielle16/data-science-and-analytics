fbef3fecfb0b681bb2f604d24fcd507af1504f25
Dani G <garciadanielle5@gmail.com>
2021-05-13 20:39:52 -0400

    adding javascript homework

---
+++ Homework/14-Intro-To-Javascript/UFO-level-1/static/js/app.js
@@ -0,0 +1,53 @@
+// from data.js
+var tableData = data;
+
+var tbody = d3.select("tbody");
+var inputFieldOne = d3.select("#datetime");
+var inputFieldTwo = d3.select("#city");
+var button = d3.select("#filter-btn");
+var resetbutton = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var populate = (dataInput) => {
+
+	dataInput.forEach(ufo_sightings => {
+		var row = tbody.append("tr");
+		columns.forEach(column => row.append("td").text(ufo_sightings[column])
+		)
+	});
+}
+
+populate(data);
+
+button.on("click", () => {
+	d3.event.preventDefault();
+	var inputDate = inputFieldOne.property("value").trim();
+    var inputCity = inputFieldTwo.property("value").toLowerCase().trim();
+    
+	var filterDate = data.filter(data => data.datetime === inputDate);
+    var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+    var filterCity = data.filter(data => data.city === inputCity);
+    
+	tbody.html("");
+
+	let response = {
+		filterData, filterCity, filterDate
+	}
+
+	if (response.filterData.length !== 0) {
+		populate(filterData);
+	}
+		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
+			populate(filterCity) || populate(filterDate);
+	
+		}
+		else {
+			tbody.append("tr").append("td").text("No results found!"); 
+		}
+})
+
+resetbutton.on("click", () => {
+	tbody.html("");
+	populate(data)
+	console.log("Table reset")
+})
