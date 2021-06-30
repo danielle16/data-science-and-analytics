bd8387ff5c56ffc2fee85c312048eb4a268e604f
Dani G <garciadanielle5@gmail.com>
2021-06-30 19:24:27 -0400

    updating d3 code

--- Homework/16-D3/StarterCode/assets/js/app.js
+++ Homework/16-D3/StarterCode/assets/js/app.js
@@ -1,130 +1,105 @@
+
+// svg chart
 var svgWidth = 960;
 var svgHeight = 500;
-var margin = {top: 20, right: 40, bottom: 60, left: 100};
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+};
+
+
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-var svg = d3
-  .select('#scatter')
-  .append('svg')
-  .attr('width', svgWidth)
-  .attr('height', svgHeight)
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-var chart = svg.append('g');
+//wrapper for svg group
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-d3.csv("../data/data.csv", function(err, healthData) {
-    if(err) throw err;
-
-    healthData.forEach(function(data) {
-        data.poverty = +data.poverty;
-        data.phys_act = +data.phys_act;
-    });
-    
-    var yLinearScale = d3.scaleLinear().range([height, 0]);
-    var xLinearScale = d3.scaleLinear().range([0, width]);
-
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-
-    var xMin;
-    var xMax;
-    var yMin;
-    var yMax;
-
-    xMin = d3.min(healthData, function(data) {
-        return +data.poverty * 0.95;
+//parse and clean up data with d3
+d3.csv("./assets/data/data.csv").then(function (censusData) {
+    censusData.forEach(function (data) {
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+        // console.log(data);
     });
 
-    xMax = d3.max(healthData, function(data) {
-        return +data.poverty * 1.05;
-    });
+    //scales
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(censusData, d => d.age))
+        .range([0, width])
+        .nice()//nicer look
 
-    yMin = d3.min(healthData, function(data) {
-        return +data.phys_act * 0.98;
-    });
+    const yScale = d3.scaleLinear()
+        .domain([6, d3.max(censusData, d => d.smokes)])
+        .range([height, 0])
+        .nice()
 
-    yMax = d3.max(healthData, function(data) {
-        return +data.phys_act *1.02;
-    });
-    
-    xLinearScale.domain([xMin, xMax]);
-    yLinearScale.domain([yMin, yMax]);
+    //axes
+    const yAxis = d3.axisLeft(yScale);
+    const xAxis = d3.axisBottom(xScale);
 
 
-    var toolTip = d3
-        .tip()
-        .attr("class", "tooltip")
-        .offset([80, -60])
-        .html(function(data) {
-            var stateName = data.state;
-            var pov = +data.poverty;
-            var physAct = +data.phys_act;
-            return (
-                stateName + '<br> Poverty: ' + pov + '% <br> Physically Active: ' + physAct +'%'
-            );
-        });
-    chart.call(toolTip);
+    //append axes to chartGroup
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`).call(xAxis);
+    chartGroup.append("g").call(yAxis);
 
-    chart.selectAll("circle")
-        .data(healthData)
+    // scatter plot
+    chartGroup.selectAll("circle")
+        .data(censusData)
         .enter()
         .append("circle")
-        .attr("cx", function(data, index) {
-            return xLinearScale(data.poverty)
-        })
-        .attr("cy", function(data, index) {
-            return yLinearScale(data.phys_act)
-        })
-        .attr("r", "15")
-        .attr("fill", "lightblue")
-        .on("mouseenter", function(data) {
-            toolTip.show(data);
-        })
-        .on("mouseout", function(data, index) {
-            toolTip.hide(data);
-        });
-    
-    chart.append("text")
-        .style("text-anchor", "middle")
-        .style("font-size", "12px")
-        .selectAll("tspan")
-        .data(healthData)
+        .attr("cx", d => xScale(d.age))
+        .attr("cy", d => yScale(d.smokes))
+        .attr("r", "10")
+        .attr("stroke-width", "1")
+        .classed("stateCircle", true)
+        .attr("opacity", 0.8)
+
+    //add text attributes
+    chartGroup.append("g")
+        .selectAll('text')
+        .data(censusData)
         .enter()
-        .append("tspan")
-            .attr("x", function(data) {
-                return xLinearScale(data.poverty - 0);
-            })
-            .attr("y", function(data) {
-                return yLinearScale(data.phys_act - 0.2);
-            })
-            .text(function(data) {
-                return data.abbr
-            });
-    
-    chart
-        .append("g")
-        .attr('transform', `translate(0, ${height})`)
-        .call(bottomAxis);
-
-    chart.append("g").call(leftAxis);
-    chart
         .append("text")
+        .text(d => d.abbr)
+        .attr("x", d => xScale(d.age))
+        .attr("y", d => yScale(d.smokes))
+        .classed(".stateText", true)
+        .attr("font-family", "sans-serif")
+        .attr("text-anchor", "middle")
+        .attr("fill", "white")
+        .attr("font-size", "10px")
+        .style("font-weight", "bold")
+        .attr("alignment-baseline", "central");
+
+    //add axes titles
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
+        .attr("fill", "black")
+        .style("font-weight", "bold")
+        .text("Median Age");
+    chartGroup.append("text")
+        .attr("y", 0 - ((margin.left / 2) + 2))
+        .attr("x", 0 - (height / 2))
+        .attr("text-anchor", "middle")
+        .attr("font-size", "14px")
+        .attr("fill", "black")
+        .style("font-weight", "bold")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0-margin.left + 40)
-        .attr("x", 0 - height/2)
-        .attr("dy","1em")
-        .attr("class", "axis-text")
-        .text("Physically Active (%)")
+        .text("Smokers (%)");
 
-    chart
-        .append("text")
-        .attr(
-            "transform",
-            "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")"
-        )
-        .attr("class", "axis-text")
-        .text("In Poverty (%)");
+})
+    .catch(function (error) {
+        console.log(error)
     });
